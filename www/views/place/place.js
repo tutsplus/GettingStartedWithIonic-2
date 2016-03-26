93588db23cbe20a9687943d99ad500cc4fd3cd8b
Jeremy Wilken <jeremy.wilken@teradata.com>
2016-03-26 14:30:59 -0500

    add place view

---
+++ www/views/place/place.js
@@ -0,0 +1,48 @@
+angular.module('App')
+.config(function($stateProvider) {
+  $stateProvider.state('place', {
+    url: '/places/:place_id',
+    controller: 'PlaceController as vm',
+    templateUrl: 'views/place/place.html',
+    resolve: {
+      Place: function($http, $stateParams) {
+        var url = 'https://civinfo-apis.herokuapp.com/civic/place?place_id=' + $stateParams.place_id;
+        return $http.get(url);
+      }
+    }
+  });
+})
+.controller('PlaceController', function($scope, $stateParams, $http, $ionicLoading, $ionicActionSheet, Place) {
+  var vm = this;
+
+  $scope.$on('$ionicView.afterEnter', function() {
+    $ionicLoading.hide();
+  });
+
+  vm.photo_base = 'https://civinfo-apis.herokuapp.com/civic/photo?photo_id=';
+  vm.place = Place.data.result;
+
+  vm.openSheet = function() {
+    var sheet = $ionicActionSheet.show({
+      titleText: 'Share this place',
+      buttons: [
+        { text: 'Share via Twitter' },
+        { text: 'Share via Facebook' },
+        { text: 'Share via Email'}
+      ],
+      cancelText: 'Cancel',
+      buttonClicked: function(index) {
+        if (index === 0) {
+          window.open('https://twitter.com/intent/tweet?text=' +
+            encodeURIComponent('I found this great place! ' + vm.place.url));
+        } else if (index === 1) {
+          window.open('https://www.facebook.com/sharer/sharer.php?u=' + vm.place.url);
+        } else if (index === 2) {
+          window.open('mailto:?subject=' + encodeURIComponent('I found this great place!') + '&body=' + vm.place.url);
+        }
+        sheet();
+      }
+    });
+  };
+
+});
